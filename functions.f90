61a0381ba9f9715dc497e1d22274c3ebcaeb60ab
Eugene Smirnov <smirik@gmail.com>
2010-11-01 21:18:42 +0300

    Some functions in test.f90 file

--- functions.f90
+++ functions.f90
@@ -8,3 +8,17 @@ end subroutine squarize
 function square(x)
   square = x**2
 end function
+
+recursive function nfactorial(n) result (fac) 
+  ! computes the factorial of n (n!)
+  integer :: fac
+  integer, intent(in) :: n
+  
+  fac = 0.0
+  select case (n) 
+    case (0:1)
+      fac = 1 
+    case default
+      fac = n * nfactorial (n-1) 
+  end select
+end function nfactorial
