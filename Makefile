f4ca60a2e63f159e095c3cd5bfff4223c4debf98
Evgeny Smirnov <smirik@gmail.com>
2011-03-09 13:18:17 +0300

    Creating fortran copy of calc.rb

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-calc: calc.o mercury_parser.o
-	gfortran -o calc calc.o mercury_parser.o
+calc: calc.o mercury_parser.o vendor/strings/stringmod.o
+	gfortran -o calc calc.o mercury_parser.o  axis/functions.o vendor/strings/stringmod.o
 
-mercury_parser.o: mercury_parser.f90  
-	gfortran -c mercury_parser.f90
+mercury_parser.o: mercury_parser.f90
+	gfortran -c axis/functions.o mercury_parser.f90
 
 calc.o: calc.f90
 	gfortran -c calc.f90
--- mercury_parser.f90
+++ mercury_parser.f90
@@ -19,3 +19,22 @@ function parse_data_string (s)
 !   
   parse_data_string = 1.0
 end function parse_data_string
+
+function calc_resonance_argument(data_j, data_s, data_)
+
+end function calc_resonance_argument
+
+! data: 1 — time, 2 — longitude of perihelium, 3 — mean anomaly, 4 — semimajor axis
+function calc_resonance_parameters(data)
+  real, dimension(5) :: calc_resonance_parameters
+  real, dimension(5) :: c_data ! 1 — time, 2 — mean longitude, 3 — longitude of perihelium, 4 — mean motion, 5 — s/m axis
+  
+  c_data(1) = data(1)
+  c_data(2) = data(2) + data(3)
+  c_data(3) = data(2)
+  c_data(4) = n_from_a(data(4))
+  c_data(5) = data(4)
+  
+  calc_resonance_parameters = c_data
+  
+end function calc_resonance_parameters
