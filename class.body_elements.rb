6ed09eb415e4e92425749106e5ca22d24d58eb7d
Evgeny Smirnov <smirik@gmail.com>
2011-04-16 16:10:51 +0400

    Delete fortran files, add old ruby files for resonance table

---
+++ class.body_elements.rb
@@ -0,0 +1,96 @@
+class Body
+  
+  attr_accessor :axis, :eccentricity, :parameter, :square_constant, :inclination,
+                :true_anomaly, :argument_of_periapsis, :g,
+                :eccentric_anomaly, :mean_anomaly, :longitude_node,
+                :ecliptic_longitude, :ecliptic_latitude, :argument_of_latitude, 
+                :mean_motion, :perihelion_longitude
+
+  def mean_motion_from_axis()
+    @mean_motion = Math.sqrt(CONFIG['constants']['k2'] / (@axis**3))
+  end
+  
+  def axis_from_mean_motion()
+    @axis = (CONFIG['constants']['k'] / (@mean_motion))**(2.0/3)
+  end
+
+  def full_pp
+    puts "a = #{@axis}, ecc = #{@eccentricity}, parameter = #{parameter}, mean motion = #{mean_motion}"
+    puts "true anomaly = #{@true_anomaly}, node longitude = #{longitude_node}, incl = #{inclination}"
+    puts "mean anomaly = #{@mean_anomaly}, square const = #{@square_constant}, ecliptic longitude = #{ecliptic_longitude}"
+    puts "argument of latitude = #{@argument_of_latitude}, eclipt. latitude = #{ecliptic_latitude}"
+    puts "eccentric_anomaly = #{eccentric_anomaly}, argument of periapsis = #{argument_of_periapsis}"
+    raise
+  end
+  
+  def from(pos, vel)
+    @pos = pos
+    @vel = vel
+    
+    self.to_elem
+  end
+  
+  def to_elem
+    kappa2 = CONFIG['constants']['u']
+    kappa  = Math.sqrt(kappa2)
+
+    c_tmp = 1731.45833
+
+    # calculating orbit plane
+    
+    a_big = @pos[1]*@vel[2]-@pos[2]*@vel[1]
+    b_big = @pos[2]*@vel[0]-@pos[0]*@vel[2]
+    c_big = @pos[0]*@vel[1]-@pos[1]*@vel[0]
+    
+    dist = Math.sqrt(a_big**2+b_big**2+c_big**2)
+    @square_constant = dist
+    
+    a_small = a_big/dist
+    b_small = b_big/dist
+    c_small = c_big/dist
+    
+    ecc_v = @pos*@vel.module/kappa - @vel*(@pos*@vel)/@pos.module - @pos/@pos.module
+    
+    #puts "A = #{a_big}, B = #{b_big}, C = #{c_big}"
+    #puts "a= #{a_small}, b = #{b_small}, c = #{c_small}"
+    
+    # calculating a, e, n, c
+    @axis = @pos.module/(2.0-@pos.module*((@vel.module)**2)/kappa2)
+    @parameter = @square_constant**2/kappa2
+    @eccentricity = Math.sqrt(1.0-@parameter/@axis)
+    @mean_motion = Math.sqrt(kappa2 / (@axis**3))
+    
+    # calculating i, omega, u
+    @inclination    = Math.acos(c_small)
+    @longitude_node = Math.acos(-b_small/(Math.sqrt(a_small**2+b_small**2)))
+    if (a_big < 0)
+      @longitude_node = 2*Math::PI - @longitude_node
+    end
+    @ecliptic_latitude = Math.asin(@pos[2]/@pos.module)
+    @ecliptic_longitude = Math.acos(-@pos[0]/Math.sqrt(@pos[0]**2+@pos[1]**2))
+    @argument_of_latitude = Math.asin(@pos[2]/(@pos.module*Math.sin(@inclination)))
+    # another
+    # Math.asin(a_small/(Math.sqrt(a_small**2+b_small**2))) 
+    
+    @true_anomaly = Math.acos(1.0/@eccentricity*(@parameter/@pos.module-1.0))
+    if (@pos*@vel < 0)
+      @true_anomaly = 2*Math::PI - @true_anomaly
+    end
+    @argument_of_periapsis = @true_anomaly - @argument_of_latitude
+    #if (ecc_v[2] < 0)
+      #@argument_of_periapsis = 2*Math::PI - @argument_of_periapsis
+    #end
+    @eccentric_anomaly = Math.acos((@eccentricity + Math.cos(@true_anomaly))/(1.0+@eccentricity*Math.cos(@true_anomaly)))
+    @mean_anomaly = @eccentric_anomaly - @eccentricity*Math.sin(@eccentric_anomaly)
+    
+    @perihelion_longitude = @perihelion_longitude + @argument_of_periapsis
+        
+  end 
+  
+end
+
+class Numeric
+  def to_grad
+    self*180.0/Math::PI
+  end
+end
---
+++ main.rb
@@ -0,0 +1,66 @@
+include Math
+
+require 'yaml'
+require 'classes/class.body.rb'
+
+CONFIG = YAML.load_file('config/config.yml')
+
+def count_axe(resonance, jupiter, saturn, asteroid)
+  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.perihelion_longitude-resonance[4]*saturn.perihelion_longitude)/resonance[2]
+  if (asteroid.mean_motion < 0)
+    return false
+  end
+  asteroid.axis = asteroid.axis_from_mean_motion
+  #puts asteroid.axis
+
+  eps = (jupiter.axis - asteroid.axis)/jupiter.axis
+  asteroid.perihelion_longitude = CONFIG['constants']['k']/(2*Math::PI)*Math.sqrt(asteroid.axis/jupiter.axis)*(eps**2)*jupiter.mean_motion
+
+  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.perihelion_longitude-resonance[4]*saturn.perihelion_longitude-resonance[2]*asteroid.perihelion_longitude)/resonance[2]
+  asteroid.axis = asteroid.axis_from_mean_motion
+  asteroid.axis
+  #puts "axe for [#{resonance[0]}, #{resonance[1]}, #{resonance[2]}, 0, 0, #{resonance[5]}] = #{asteroid.axis}"
+end
+
+jupiter  = Body.new
+saturn   = Body.new
+asteroid = Body.new 
+
+jupiter.axis = CONFIG['constants']['axis'][5]
+saturn.axis  = CONFIG['constants']['axis'][6]
+
+jupiter.mean_motion = CONFIG['constants']['mean_motion'][5].from_sec_to_rad_year
+saturn.mean_motion  = CONFIG['constants']['mean_motion'][6].from_sec_to_rad_year
+
+jupiter.perihelion_longitude = CONFIG['constants']['perihelion_longitude'][5].from_sec_to_rad_year
+saturn.perihelion_longitude  = CONFIG['constants']['perihelion_longitude'][6].from_sec_to_rad_year
+
+# resonance = [5, -2, -2, 0, 0, 1]
+
+max_order = 4
+count  = 0
+count2 = 0
+
+for i in 1..8
+  for j in -max_order..max_order
+    for k in -max_order..max_order
+      diff = (0.0 - i - j - k).to_i
+      resonance = [i,j,k,0,0,diff]
+      if ((i == 0) || (j == 0) || (k == 0))
+        next
+      end
+      tmp = count_axe(resonance, jupiter, saturn, asteroid)
+      if ((diff.abs < max_order) && (asteroid.axis > 1.5) && (asteroid.axis < 5) )
+        count += 1
+        #puts "#{resonance[0]}; #{resonance[1]}; #{resonance[2]}; 0; 0; #{resonance[5]}; #{asteroid.axis}"
+        # TeX
+        if (asteroid.axis != 0)
+          printf("%d & %d & %d & %d & %2.4f \\\\ \n", resonance[0], resonance[1], resonance[2], resonance[5].abs, asteroid.axis)
+        end
+      end
+    end
+  end
+end
+
+puts "total = #{count}, #{count2}"
+
