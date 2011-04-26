314ec4b713fd881ae9899d2f2bae8255493656f2
Evgeny Smirnov <smirik@gmail.com>
2011-04-27 00:55:17 +0400

    Fix to_i bug in cron

--- cron.rb
+++ cron.rb
@@ -4,9 +4,18 @@ require 'classes/functions.rb'
 require 'classes/resonance_archive.rb'
 
 start = get_command_line_argument('start', false)
-steps = get_command_line_argument('step', false)
+steps = get_command_line_argument('steps', false)
 
+if (!start || !steps)
+  raise
+end
+
+start = start.to_i
+steps = steps.to_i
+
+puts "Start"
 for i in 0..steps
   num = start + 100*i
   tmp = %x[ ruby resonance.rb --start=#{num}  --integrate=1; ruby packager.rb --start=#{num} --zip=1 ]
 end
+puts "End"
