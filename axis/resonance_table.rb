6adf1cac5069b6f795443db1d785186754b4f199
Evgeny Smirnov <smirik@gmail.com>
2011-09-11 20:03:21 +0400

    Fix bug in longitude_of_periapsis naming, change resonance matrix generator

--- axis/resonance_table.rb
+++ axis/resonance_table.rb
@@ -7,7 +7,7 @@ CONFIG = YAML.load_file('config/config.yml')
 debug = CONFIG['debug']
 
 def count_axe(resonance, jupiter, saturn, asteroid)
-  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.perihelion_longitude-resonance[4]*saturn.perihelion_longitude)/resonance[2]
+  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.longitude_of_periapsis-resonance[4]*saturn.longitude_of_periapsis)/resonance[2]
   if (asteroid.mean_motion < 0)
     return false
   end
@@ -15,9 +15,9 @@ def count_axe(resonance, jupiter, saturn, asteroid)
   #puts asteroid.axis
 
   eps = (jupiter.axis - asteroid.axis)/jupiter.axis
-  asteroid.perihelion_longitude = CONFIG['constants']['k']/(2*Math::PI)*Math.sqrt(asteroid.axis/jupiter.axis)*(eps**2)*jupiter.mean_motion
+  asteroid.longitude_of_periapsis = CONFIG['constants']['k']/(2*Math::PI)*Math.sqrt(asteroid.axis/jupiter.axis)*(eps**2)*jupiter.mean_motion
 
-  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.perihelion_longitude-resonance[4]*saturn.perihelion_longitude-resonance[2]*asteroid.perihelion_longitude)/resonance[2]
+  asteroid.mean_motion = (-resonance[0]*jupiter.mean_motion-resonance[1]*saturn.mean_motion-resonance[3]*jupiter.longitude_of_periapsis-resonance[4]*saturn.longitude_of_periapsis-resonance[2]*asteroid.longitude_of_periapsis)/resonance[2]
   asteroid.axis = asteroid.axis_from_mean_motion
   asteroid.axis
   #puts "axe for [#{resonance[0]}, #{resonance[1]}, #{resonance[2]}, 0, 0, #{resonance[5]}] = #{asteroid.axis}"
@@ -33,14 +33,14 @@ saturn.axis  = CONFIG['constants']['axis'][6]
 jupiter.mean_motion = CONFIG['constants']['mean_motion'][5].from_sec.from_day_to_year
 saturn.mean_motion  = CONFIG['constants']['mean_motion'][6].from_sec.from_day_to_year
 
-jupiter.perihelion_longitude = CONFIG['constants']['perihelion_longitude'][5].from_sec.from_day_to_year
-saturn.perihelion_longitude  = CONFIG['constants']['perihelion_longitude'][6].from_sec.from_day_to_year
+jupiter.longitude_of_periapsis = CONFIG['constants']['longitude_of_periapsis'][5].from_sec.from_day_to_year
+saturn.longitude_of_periapsis  = CONFIG['constants']['longitude_of_periapsis'][6].from_sec.from_day_to_year
 
 format = CONFIG['resonance_table']['format']
 
 # resonance = [5, -2, -2, 0, 0, 1]
 
-max_order = 4
+max_order = 7
 count  = 0
 count2 = 0
 
@@ -49,9 +49,8 @@ for i in 1..8
     for k in -max_order..max_order
       diff = (0.0 - i - j - k).to_i
       resonance = [i,j,k,0,0,diff]
-      if ((i == 0) || (j == 0) || (k == 0))
-        next
-      end
+      next if ((i == 0) || (j == 0) || (k == 0))
+      next if (diff.abs > max_order)
       tmp = count_axe(resonance, jupiter, saturn, asteroid)
       if ((diff.abs < max_order) && (asteroid.axis > 1.5) && (asteroid.axis < 5) )
         count += 1
