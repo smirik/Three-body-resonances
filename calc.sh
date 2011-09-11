943619e75d6c809f00adc9b50c85cdeb062fb805
Evgeny Smirnov <smirik@gmail.com>
2011-09-11 20:46:51 +0400

    Delete old files

--- calc.sh
+++
@@ -1,8 +0,0 @@
-start=$1
-stop=$2
-
-for (( i=$start; i<$stop; i=i+100 ))
-do
-  let next=i+100
-  ruby calc.rb --start=$i --stop=$next
-done
--- extractor.rb
+++
@@ -1,9 +0,0 @@
-# Extract integrator files from archive
-require 'yaml'
-require 'classes/functions.rb'
-require 'classes/resonance_archive.rb'
-
-start  = get_command_line_argument('start', false)
-elements = get_command_line_argument('elements', false)
-
-ResonanceArchive.extract(start, elements)
